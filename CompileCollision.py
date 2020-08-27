8cb5b7ac020240c5109b3653c215507ee075cf1d
hoskar <vorona07@mail.ru>
2020-08-27 13:12:39 +0300

    1

--- CompileCollision.py
+++ CompileCollision.py
@@ -29,14 +29,35 @@ def turn_car(event):
     w.itemconfig(img, image=image_tk)
     compute_collision_points()
 
+def turn_car_left(event):
+    global ANGLE, image_tk, img
+    ANGLE -= 10
+    ANGLE %= 360
+
+    image_tmp = def_image.rotate(ANGLE, expand=True)
+    image_tk = ImageTk.PhotoImage(image_tmp)
+    # w.create_image(250, 250, image=image_tk)
+    w.itemconfig(img, image=image_tk)
+    compute_collision_points()
+
 
 w.bind('<Button-1>', turn_car)
-
+w.bind('<Button-3>', turn_car_left)
 
 def draw_point(x, y):
     radius = 5
     point = w.create_oval(x - radius, y - radius, x + radius, y + radius, fill="green")
     return point
+
+
+def turn_point_by_angle(angle, center_x, center_y, x, y):
+    new_x = (x - center_x) * math.sin(angle * math.pi / 180) - (
+            (y - center_y) * math.cos(angle * math.pi / 180)) + center_x
+
+    new_y = (x - center_x) * math.cos(angle * math.pi / 180) + (
+            (y - center_y) * math.sin(angle * math.pi / 180)) + center_y
+
+    return new_x, new_y
 
 
 def compute_collision_points():
@@ -54,15 +75,15 @@ def compute_collision_points():
         centery = coord[1]
         x = i[0]
         y = i[1]
+        #
+        # newx = (x - centerx) * math.sin(ANGLE * math.pi / 180) - (
+        #         (y - centery) * math.cos(ANGLE * math.pi / 180)) + centerx
+        #
+        # newy = (x - centerx) * math.cos(ANGLE * math.pi / 180) + (
+        #         (y - centery) * math.sin(ANGLE * math.pi / 180)) + centery
 
-        newx = (x - centerx) * math.sin(ANGLE * math.pi / 180) - (
-                (y - centery) * math.cos(ANGLE * math.pi / 180)) + centerx
-
-        newy = (x - centerx) * math.cos(ANGLE * math.pi / 180) + (
-                (y - centery) * math.sin(ANGLE * math.pi / 180)) + centery
-
-        collision_points.append(draw_point(newx, newy))
-
+        # collision_points.append(draw_point(newx, newy))
+        collision_points.append(draw_point(*turn_point_by_angle(ANGLE, centerx, centery, x, y)))
 
 compute_collision_points()
 
